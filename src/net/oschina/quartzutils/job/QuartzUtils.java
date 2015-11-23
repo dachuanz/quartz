26660addfbaba8b2d7b298547dddc46c83922911
dachuanz <dachuanz@gmail.com>
2015-11-23 15:19:56 +0800

    Update QuartzUtils.java

--- src/net/oschina/quartzutils/job/QuartzUtils.java
+++ src/net/oschina/quartzutils/job/QuartzUtils.java
@@ -96,7 +96,7 @@ public class QuartzUtils {
 		logger.debug(scheduler
 				+ ".......................................................................................standby");
 		scheduler.standby();
-		;
+		
 
 	}
 
